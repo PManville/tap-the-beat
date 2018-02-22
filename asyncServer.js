32ecf420d20c0eb0ddbd1e46d641d1d2cee9e54d
patrickmanville <patrickmanville@gmail.com>
2018-02-22 13:27:41 +0100

    v1 amends

---
+++ asyncServer.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const shortId = require('shortid');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = 8888;
+
+app.get(`/restartGame`, function(req, res) {
+	res.send({
+		bpmCurrent: 0,
+		currentTime: Date.now(),
+		started: false,
+		time: 30
+	});
+});
+
+const errorResponse = (res, status, text) => {
+	res.status(status);
+	res.send({ error: text });
+}
+
+app.listen(PORT, function () {
+  console.log(`Async request server now running on port ${PORT}.`);
+});
